a94311b078359e1b3f62f736def60b776832d88e
Rafał Krupiński <rafiwts@gmail.com>
2023-04-08 22:31:16 +0200

    Adding views and urls for registering a user and validating credentials

--- bookmarks/account/models.py
+++ bookmarks/account/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Profile(models.Model): # profile will be connected to a user
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    date_of_birth = models.DateField(blank=True, null=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d',
+                             blank=True)
+    
+    def __str__(self):
+        return f'{self.user.username} profile'
--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     # path('login/', views.user_login, name='login'),
-    path('', include('django.contrib.auth.urls')) # for methods like login, logout, logout_then_login etc - it looks for registration folder (with html files), see notes
+    path('', include('django.contrib.auth.urls')), # for methods like login, logout, logout_then_login etc - it looks for registration folder (with html files), see notes
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit_profile, name='edit')
 ]
