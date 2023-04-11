78f7fff124ee48e794653550d988d9d35152676b
Rafał Krupiński <rafiwts@gmail.com>
2023-04-11 17:13:45 +0200

    A new aplication has been added

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -36,12 +36,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'account.apps.AccountConfig',
+    'images.apps.ImagesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'easy_thumbnails',
 ]
 
 MIDDLEWARE = [
