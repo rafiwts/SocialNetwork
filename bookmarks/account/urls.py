609f9324408f897dbedda7879ecd8934d490e60d
Rafał Krupiński <rafiwts@gmail.com>
2023-04-12 22:53:13 +0200

    A path for displaying a user photos

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('', include('django.contrib.auth.urls')), # for methods like login, logout, logout_then_login etc - it looks for registration folder (with html files), see notes
     path('register/', views.register, name='register'),
-    path('edit/', views.edit_profile, name='edit')
+    path('edit/', views.edit_profile, name='edit'),
+    path('users/', views.user_list, name='user_list'),
+    path('users/<username>/', views.user_detail, name='user_detail'),
 ]
