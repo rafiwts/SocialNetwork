f63dea4d05cf17eadea25bf69249e907ab31d325
Rafał Krupiński <rafiwts@gmail.com>
2023-04-13 23:37:42 +0200

    Creatig views for an image and list of images

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.shortcuts import get_object_or_404
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Image
 from .forms import ImageCreateForm
 
@@ -30,3 +33,46 @@ def image_create(request):
                   'images/image/create.html',
                   {'section': 'images',
                    'form': form})
+
+
+@login_required
+@require_POST # only for a post method (we have also require_GET)
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action') # like or unlike
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status':'ok'})
+        except:
+            pass
+    return JsonResponse({'status':'ok'}) # it returns a velue in JSON
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+    if images_only:
+        return render(request,
+                      'images/image/list_images.html',
+                      {'section': 'images',
+                       'images': images})
+    return render(request,
+                 'images/image/list.html',
+                 {'section': 'images',
+                  'images': images})
