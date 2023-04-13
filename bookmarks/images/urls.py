4b511d15dae20313d4542cb95f7b2c72f72981be
Rafał Krupiński <rafiwts@gmail.com>
2023-04-13 23:38:30 +0200

    Creating paths for images

--- bookmarks/images/urls.py
+++ bookmarks/images/urls.py
@@ -3,6 +3,8 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('create/', views.image_create, name='create'),
-    path('detail/<int:id>/<slug:slug>', views.image_detail, name='detail')
+    path(r'create/', views.image_create, name='create'),
+    path(r'detail/<int:id>/<slug:slug>', views.image_detail, name='detail'),
+    path(r'like/', views.image_like, name="like"),
+    path(r'', views.image_list, name='list')
 ]
