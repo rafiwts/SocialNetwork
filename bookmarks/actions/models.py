3c290a6aa02c8b2bcb2aacbbb87860f067489acd
Rafał Krupiński <rafiwts@gmail.com>
2023-04-22 21:14:15 +0200

    Creating a model for user's actions

--- bookmarks/actions/models.py
+++ bookmarks/actions/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+
+class Action(models.Model):
+    user = models.ForeignKey('auth.User',
+                             related_name='actions',
+                             on_delete=models.CASCADE)
+    verb = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    target_ct = models.ForeignKey(ContentType, # it leads to a ContentType model
+                                  blank=True,
+                                  null=True,
+                                  related_name='target_obj',
+                                  on_delete=models.CASCADE)
+    target_id = models.PositiveBigIntegerField(null=True, # the key for the object
+                                               blank=True)
+    target = GenericForeignKey('target_ct', 'target_id') # the key for the relation between a model and an object
+
+    class Meta:
+        ordering = ('-created',)
+
+
