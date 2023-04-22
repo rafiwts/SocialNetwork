4b61fe633ea0a700b7690c2f27b82ae6d6c9a518
Rafał Krupiński <rafiwts@gmail.com>
2023-04-22 20:36:58 +0200

    Adding a path for following a user

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     # path('login/', views.user_login, name='login'),
     path('', include('django.contrib.auth.urls')), # for methods like login, logout, logout_then_login etc - it looks for registration folder (with html files), see notes
+    path('myblog/', views.my_blog, name='my_blog'),
     path('register/', views.register, name='register'),
     path('edit/', views.edit_profile, name='edit'),
     path('users/', views.user_list, name='user_list'),
-    path('users/<username>/', views.user_detail, name='user_detail'),
+    path('users/follow/', views.user_follow, name='user_follow'),
+    path('users/<username>/', views.user_detail, name='user_detail'),   
 ]
