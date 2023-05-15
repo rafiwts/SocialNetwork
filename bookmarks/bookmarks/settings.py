af407fac3bd7b3579da2795c4f9c3411297f2779
Rafał Krupiński <rafiwts@gmail.com>
2023-05-15 20:14:44 +0200

    A secret key has been moved to a .env file

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sb47lfdeq2y9a6&gk+r$!n&(b++b+&wrs&)zqfx3z8ad476l!l'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
